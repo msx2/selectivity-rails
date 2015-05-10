0d1f27cb161ef04a4f2981ad428c489e9be3f1f9
Konrad Jurkowski <konrad@jurkowski.me>
2015-05-10 16:20:35 +0200

    Updated dummy test file for Hound

--- hound_test.rb
+++ hound_test.rb
@@ -1,2 +1,10 @@
-puts 'something'
-puts "something #{'else'}"
+puts 'single quotes'
+puts "double quotes"
+
+puts "this is #{'interpolation'}"
+
+puts "this " + "is " + "an " + "concatenation"
+puts "this " "also " "is " "an " "concatenation"
+
+puts 'this ' + 'is ' + 'an ' + 'concatenation'
+puts 'this ' 'also ' 'is ' 'an ' 'concatenation'
